965c9c0151c1391adf72942225cb923024c4074a
Adam Albarisyi <adam@baleomol.dev>
2023-09-07 15:49:47 +0700

    checkpoint section qualification

--- portofolio-3/src/App.js
+++ portofolio-3/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <main className="main">
         <Home/>
         <About/>
+        <Skills/>
+        <Services/>
+        <Qualification/>
       </main>
     </>
   );
